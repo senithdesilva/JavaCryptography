74ca2b2a1d9a44f58f06d2645aadf9f7e9b40972
Senith De Silva <senith.desilva@yahoo.co.uk>
2019-01-13 11:32:20 +1100

    Initialization Vector
    Created a method to generate the Initialization Vector to encrypt the plain text

--- src/main/java/com/crypto/symmetric/SymmetricEncryption.java
+++ src/main/java/com/crypto/symmetric/SymmetricEncryption.java
@@ -8,7 +8,13 @@ public class SymmetricEncryption {
     // Advanced Encryption Standard
     private static final String AES = "AES";
 
-    public static SecretKey createAESKey() throws Exception{
+    // CBC: Cipher Block Chaining
+    // Padding of 5 and PKCS, which is a padding scheme
+    // Here, we are going to pass in a block cipher
+    private static final String AES_CIPHER_ALGORITHM = "AES/CBC/PKCS5Padding";
+
+    // Create a random symmetric key
+    static SecretKey createAESKey() throws Exception{
         // Create a SecureRandom instance
         // In the form of a pseudo-random number generator (PRNG)
         // They use a deterministic algorithm to produce a pseudo-random sequence from a true random seed
@@ -19,5 +25,19 @@ public class SymmetricEncryption {
         KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
         keyGenerator.init(256, secureRandom);
         return keyGenerator.generateKey();
+    }
+
+    // To make each message unique, an Initialization Vector must be used in the first block.
+    // Initialization Vector is the first step used to encrypt the plain text.
+    public static byte[] createInitializationVector(){
+        // With a size of 16
+        byte[] initializationVector = new byte[16];
+
+        SecureRandom secureRandom = new SecureRandom();
+
+        // This will populate all 16 bytes randomly.
+        secureRandom.nextBytes(initializationVector);
+
+        return initializationVector;
     }
 }
